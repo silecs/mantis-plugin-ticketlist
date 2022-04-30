2e1d198270847dd0833309e654c4b4590a765c6c
François Gannaz <francois.gannaz@silecs.info>
2022-04-30 11:08:01 +0200

    Button to refresh the table of issues

--- js/views/current-list/TicketsMainBlock.js
+++ js/views/current-list/TicketsMainBlock.js
@@ -1,4 +1,5 @@
 import m from "mithril"
+import List from "../../models/List";
 import Tickets from "../../models/Tickets";
 import TicketsBlock from "./TicketsBlock";
 import WidgetBox from "./WidgetBox";
@@ -6,7 +7,34 @@ import WidgetBox from "./WidgetBox";
 const BlockTitle = {
     view(vnode) {
         const tickets = Tickets.get()
-        return `Tickets listés (${tickets.length})`
+        return [
+            `Tickets listés (${tickets.length})`,
+            m(RefreshButton, {num: tickets.length}),
+        ];
+    },
+}
+
+const RefreshButton = {
+    loading: false,
+    view(vnode) {
+        if (vnode.attrs.num == 0) {
+            return null
+        }
+        return m('button',
+            {
+                class: 'btn btn-default btn-sm',
+                title: "Relire les états de ces tickets sur le serveur",
+                type: 'button',
+                onclick: () => {
+                    this.loading = true
+                    Tickets.load(List.getTicketIds(), List.get().projectId).then(() => {
+                        this.loading = false
+                    });
+                },
+                disabled: this.loading,
+            },
+            this.loading ? m('i.fa.fa-spinner') : m('i.fa.fa-refresh')
+        )
     },
 }
 
